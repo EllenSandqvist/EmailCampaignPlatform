b5ac30505a783f69b706e4f694f84e0a7ec4f256
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-11-01 15:59:54 +0100

    Added console log in requireAuth

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express-serve-static-core";
 
 const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.user);
   if (req.isAuthenticated()) {
     return next();
   } else {
