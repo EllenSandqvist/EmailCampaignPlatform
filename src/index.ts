1ca9cd60e16f2f81b405c7effdc593efd47f3c16
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-11-05 15:02:44 +0100

    Added trust proxy setting
    Co-authored-by: Regni <Regni@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ const allowedOrigins =
   process.env.NODE_ENV === "production"
     ? ["https://main.d2iuui4uss8fdt.amplifyapp.com", "http://localhost:3000"]
     : ["http://localhost:5173", "http://localhost:3000"];
-
+//this is for vercel to trust the proxy deployment.
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
